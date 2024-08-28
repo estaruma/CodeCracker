1b8d781d7e54e9a1e95dfa64a4572b041487652b
estaruma <esther.arumainayagam@mettle.co.uk>
2024-08-28 15:20:00 +0100

    add main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        char[] decryptionKey = {'!', ')', '"', '(', '£', '*', '%', '&', '>', '<', '@', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o'};
+
+        CodeCracker codeCracker = new CodeCracker(decryptionKey);
+        CipherValidator cipherValidator = new CipherValidator();
+
+        String message = "Hello World!";
+        if (cipherValidator.isValidMessage(message)) {
+
+            String encoded = codeCracker.encodeMessage(message);
+            System.out.println(encoded);
+
+            String decoded = codeCracker.decodeMessage(encoded);
+            System.out.println(decoded);
+        } else {
+            System.out.println("Invalid message");
+        }
+
+
     }
 }
