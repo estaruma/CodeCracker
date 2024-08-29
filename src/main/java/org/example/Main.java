ac24b63bdfefe303b34576409886e5c9589bca94
estaruma <esther.arumainayagam@mettle.co.uk>
2024-08-29 15:24:23 +0100

    updated Encrypted and decrypted class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,19 +3,18 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        String[] decryptionKey = {"!", ")", "\"", "(", "£", "*", "%", "&", ">", "<", "@", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o"};
+        String alphabet = "abcdefghijklmnopqrstuvwxyz !";
+        String[] decryptionKey = {"!", ")", "\"", "(", "£", "*", "%", "&", ">", "<", "@", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", " ", "!"};
 
-
-        AlphabetHelper mapping = new AlphabetHelper (alphabet, decryptionKey);
+        AlphabetHelper mapping = new AlphabetHelper(alphabet, decryptionKey);
 
         Encryptor encryptor = new Encryptor(mapping);
         Decryptor decryptor = new Decryptor(mapping);
 
-        String message = "Hello World";
+        // Convert the message to lowercase
+        String message = "Hello World!".toLowerCase();
         String encryptedMessage = encryptor.encrypt(message);
         String decryptedMessage = decryptor.decrypt(encryptedMessage);
-
 
         System.out.println("Original: " + message);
         System.out.println("Encrypted: " + encryptedMessage);
